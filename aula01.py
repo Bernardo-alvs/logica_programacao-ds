500798790244ca3978ce8868043b9834c6dddbd3
NycolasSenai <nycolasonrails@gmail.com>
2024-08-20 08:21:06 -0300

    meu primeiro projeto em python

---
+++ aula01.py
@@ -0,0 +1,18 @@
+print("ola mundo")
+
+nome = input('insira o nome')
+cpf = input("insira o cpf:")
+data_nascimento = int(input('insira sua data de nascimento'))
+
+nome = input('digite seu nome')
+nome = str(nome)
+print(type(nome))
+
+cpf = input('digite seu cpf:')
+cpf = int(cpf)
+print(type(cpf))
+
+data_nascimento = input('qual sua data de nascimento')
+data_nascimento = int(data_nascimento)
+print(type(data_nascimento))
+ 
---
+++ aula02.py
@@ -0,0 +1,47 @@
+'''
+
+print("ola mundo")
+
+nome = 'tricia'
+idade = 17
+peso = 55.4
+altura =1.60
+instrutor = True
+
+# FIXME: Visualizando os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(peso))
+print(type(altura))
+print(type(instrutor))
+
+
+# FIXME: Entrada de dados
+
+sobrenome = input('digite o seu sobrenome: ')
+print(nome , sobrenome)
+print(type(sobrenome))
+
+#convertendo o valor de input
+
+idade = input("digite sua idade: ")
+print(type(idade))
+idade = int(idade)
+print(type(idade))
+
+data = int(input('que dia é hoje: '))
+print(type(data))
+if data > 12:
+    print('dentro do if')
+
+'''
+
+
+
+
+
+
+
+
+
+
